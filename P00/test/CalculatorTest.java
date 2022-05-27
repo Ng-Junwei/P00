3cfbad8c40f5a978f7444d241c84cd600a707357
21010294 <21010294@10.174.114.126>
2022-05-27 14:33:52 +0800

    first push to P00 repository in Github

--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -1,4 +1,59 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class CalculatorTest {
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testAdd() {
+		int a = 4321;
+		int b = 1234;
+			
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b); 
+			 
+		int expected = 5555;
+		assertEquals (expected, actual);
+		}
+	public void testSubtract() {
+		int a = 4321;
+		int b = 1234;
+			
+		Calculator cal = new Calculator();
+		int actual = cal.subtract(a, b); 
+			 
+		int expected = 3087;
+		assertEquals (expected, actual);
+		}
+	public void testMultiply() {
+		int a = 4321;
+		int b = 1234;
+			
+		Calculator cal = new Calculator();
+		int actual = cal.multiply(a, b); 
+			 
+		int expected = 5332114;
+		assertEquals (expected, actual);
+		}
+	public void testDivide() {
+		int a = 4321;
+		int b = 1234;
+			
+		Calculator cal = new Calculator();
+		int actual = cal.divide(a, b); 
+			 
+		double expected = 3;
+		assertEquals (expected, actual);
+		}
 
 }
